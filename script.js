2074a450c5cea19b62bf9f51f1ac191e69c05764
younesbtc76-coder <younesbtc76@gmail.com>
2026-08-30 15:52:08 +0430

    Add files via upload

--- script.js
+++ script.js
@@ -9,12 +9,15 @@ function setLanguage(language) {
     document.documentElement.lang = "fa";
     document.documentElement.dir = "rtl";
     document.title = "دکتر حکمت‌الله واعظ | جراح دندان";
+    document.querySelector(".hero-text").style.gridColumn = "2";
+document.querySelector(".hero-image").style.gridColumn = "1";
   } else {
     document.documentElement.lang = "en";
     document.documentElement.dir = "ltr";
     document.title = "Dr. Hekmatullah Waez | Dental Surgeon";
-  }
-
+  document.querySelector(".hero-text").style.gridColumn = "1";
+document.querySelector(".hero-image").style.gridColumn = "2";
+}
   const buttons = document.querySelectorAll(".language-buttons button");
 
   buttons.forEach(function (button) {
@@ -33,4 +36,10 @@ function setLanguage(language) {
 document.addEventListener("DOMContentLoaded", function () {
   const savedLanguage = localStorage.getItem("siteLanguage") || "en";
   setLanguage(savedLanguage);
+});document.querySelectorAll(".comparison").forEach((comparison) => {
+  const slider = comparison.querySelector('input[type="range"]');
+
+  slider.addEventListener("input", () => {
+    comparison.style.setProperty("--position", slider.value + "%");
+  });
 });
